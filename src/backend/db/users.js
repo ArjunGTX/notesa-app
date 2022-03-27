1429c11737b57e145d2d1a1342d053475bcfd4cb
ArjunGTX <arjunatpavilion@gmail.com>
2022-03-27 12:27:54 +0530

    initial setup

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -1,17 +1,13 @@
 import { v4 as uuid } from "uuid";
 import { formatDate } from "../utils/authUtils";
-/**
- * User Database can be added here.
- * You can add default users of your wish with different attributes
- * */
 
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    email: "adarshbalika@gmail.com",
-    password: "adarshBalika123",
+    firstName: "Guest",
+    lastName: "User",
+    email: "guestuser@email.com",
+    password: "GuestUser@123",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
