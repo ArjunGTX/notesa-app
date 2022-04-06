42defcbab9647840fbd30bcea921a53b9a72aa9a
ArjunGTX <arjunatpavilion@gmail.com>
2022-04-07 00:33:49 +0530

    initialize archives and notes

--- src/contexts/ArchivesContext.jsx
+++ src/contexts/ArchivesContext.jsx
@@ -1,11 +1,31 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
+import { toast } from "react-toastify";
+import { getArchives } from "../utils/api";
+import { TOAST_ERRORS } from "../utils/constants";
+import { useAuth } from "./AuthContext";
 
 const ArchivesContext = createContext(null);
 
 export const useArchives = () => useContext(ArchivesContext);
 
 export const ArchivesProvider = ({ children }) => {
+  const { auth } = useAuth();
+
   const [archives, setArchives] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        if (!auth.isLoggedIn) return;
+        const { status, data } = await getArchives(auth.encodedToken);
+        if (status !== 200) return;
+        setArchives(data.archives);
+      } catch (error) {
+        toast.error(TOAST_ERRORS.GENERAL_ERROR);
+      }
+    })();
+  }, [auth.isLoggedIn]);
+
   return (
     <ArchivesContext.Provider value={{ archives, setArchives }}>
       {children}
--- src/contexts/NotesContext.jsx
+++ src/contexts/NotesContext.jsx
@@ -1,11 +1,30 @@
-import { useState, useContext, createContext } from "react";
+import { useState, useContext, createContext, useEffect } from "react";
+import { toast } from "react-toastify";
+import { getNotes } from "../utils/api";
+import { TOAST_ERRORS } from "../utils/constants";
+import { useAuth } from "./AuthContext";
 
 const NotesContext = createContext(null);
 
 export const useNotes = () => useContext(NotesContext);
 
 export const NotesProvider = ({ children }) => {
+  const { auth } = useAuth();
+
   const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        if (!auth.isLoggedIn) return;
+        const { status, data } = await getNotes(auth.encodedToken);
+        if (status !== 200) return;
+        setNotes(data.notes);
+      } catch (error) {
+        toast.error(TOAST_ERRORS.GENERAL_ERROR);
+      }
+    })();
+  }, [auth.isLoggedIn]);
 
   return (
     <NotesContext.Provider value={{ notes, setNotes }}>
